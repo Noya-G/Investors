ae30ca25a2331cf378d4667b6b906e25acb4cac9
noyagendelman <noiagendl@hotmail.com>
2022-05-23 15:06:24 +0300

    new classes

---
+++ bugetSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bugetSystem : MonoBehaviour
+{
+    public int budget, incomes, outcomes, transition ;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
---
+++ userStock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userStock : MonoBehaviour
+{
+    public int stockAmount, currValue,valueAtBuying;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
